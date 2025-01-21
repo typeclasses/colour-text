34ac224ed8507a0af444c84098ac092aa7fb83ec
Chris Martin <ch.martin@gmail.com>
2025-01-20 21:50:13 -0700

    bumps

--- colour-text/changelog.md
+++ colour-text/changelog.md
@@ -1,1 +1,4 @@
+
+0.0.0.1 (2025-01-20) - Version bumps
+
 0.0.0.0 (2023-05-27) - Initial release
--- flake.nix
+++ flake.nix
@@ -1,20 +1,13 @@
 {
   inputs = {
-    "nixos-22.05".url = "github:NixOS/nixpkgs/nixos-22.05";
-    "nixos-22.11".url = "github:NixOS/nixpkgs/nixos-22.11";
-    "nixos-unstable".url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs@{ self, ... }:
     let packageName = "colour-text";
     in inputs.flake-utils.lib.eachDefaultSystem (system:
       let
-        nixpkgs = {
-          "nixos-22.05" = import inputs."nixos-22.05" { inherit system; };
-          "nixos-22.11" = import inputs."nixos-22.11" { inherit system; };
-          "nixos-unstable" = import inputs."nixos-unstable" { inherit system; };
-        };
-        pkgs = nixpkgs."nixos-22.11";
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
         project = pkgs.haskellPackages.developPackage {
           root = ./colour-text;
           name = packageName;
@@ -27,56 +20,30 @@
       in {
         defaultPackage = self.packages.${system}.${packageName};
 
-        packages = {
+        packages = let
+
+          inherit (pkgs.haskell.lib) dontCheck;
+
+          makeTestConfiguration = let defaultPkgs = pkgs;
+          in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
+          let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
+          in (pkgs.haskell.packages.${ghcVersion}.override (old: {
+            overrides = combineOverrides old [
+              (packageSourceOverrides { colour-text = ./colour-text; })
+              (new: old: { })
+              overrides
+            ];
+
+          })).colour-text;
+
+        in rec {
           "${packageName}" = project;
 
-          testConfigurations = let
-
-            inherit (pkgs.haskell.lib) dontCheck;
-
-            makeTestConfiguration = let defaultPkgs = pkgs;
-            in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
-            let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
-            in (pkgs.haskell.packages.${ghcVersion}.override (old: {
-              overrides = combineOverrides old [
-                (packageSourceOverrides { colour-text = ./colour-text; })
-                (new: old: {
-                })
-                overrides
-              ];
-
-            })).colour-text;
-
-          in rec {
-            ghc-8-10 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.05";
-              ghcVersion = "ghc8107";
-            };
-            ghc-9-0 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.11";
-              ghcVersion = "ghc90";
-            };
-            ghc-9-2 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.11";
-              ghcVersion = "ghc92";
-            };
-            ghc-9-4 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.11";
-              ghcVersion = "ghc94";
-            };
-            ghc-9-6 = makeTestConfiguration {
-              ghcVersion = "ghc96";
-              pkgs = nixpkgs."nixos-unstable";
-              overrides = new: old: {
-                hspec = dontCheck (new.callPackage ./nix/hspec.nix { });
-                hspec-core = dontCheck (new.callPackage ./nix/hspec-core.nix { });
-                hspec-discover = dontCheck (new.callPackage ./nix/hspec-discover.nix { });
-              };
-            };
-            all = pkgs.symlinkJoin {
-              name = packageName;
-              paths = [ ghc-8-10 ghc-9-0 ghc-9-2 ghc-9-4 ghc-9-6 ];
-            };
+          ghc-9-6 = makeTestConfiguration { ghcVersion = "ghc96"; };
+          ghc-9-8 = makeTestConfiguration { ghcVersion = "ghc98"; };
+          all = pkgs.symlinkJoin {
+            name = packageName;
+            paths = [ ghc-9-6 ghc-9-8 ];
           };
         };
       });
